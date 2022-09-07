518a7b29b5d116ca2e114dd25f8e936269f4f635
Turganov Ivan <mrdurandall@gmail.com>
2022-09-07 22:22:25 +0300

    created user app and user model

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
