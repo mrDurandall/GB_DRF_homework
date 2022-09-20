aab2235250b340e44b13608af7213f0a7594a8df
Turganov Ivan <mrdurandall@gmail.com>
2022-09-20 21:36:20 +0300

    created todo app, added project and todo models

---
+++ todo/todoapp/models/todo.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from . import Project
+
+
+class ToDo(models.Model):
+
+    project = models.ForeignKey(
+        Project,
+        related_name='projects',
+        verbose_name='Проект',
+        on_delete=models.CASCADE
+    )
+
+    text = models.TextField(
+        verbose_name='Описание',
+    )
+
+    created_on = models.DateTimeField(
+        verbose_name='Время создания',
+        auto_now_add=True,
+    )
+
+    updated_on = models.DateTimeField(
+        verbose_name='Время последнего изменения',
+        auto_now=True,
+    )
+
+    is_active = models.BooleanField(
+        verbose_name='Заметка активна',
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'замета от {self.created_on}'
