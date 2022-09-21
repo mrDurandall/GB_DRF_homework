dfd419b1fe168f28c150debc57e19963a630ff06
Turganov Ivan <mrdurandall@gmail.com>
2022-09-21 22:53:08 +0300

    updated viewset for todos: added filter by project, pagination and override destroy method

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,5 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import FilterSet, CharFilter
@@ -22,8 +24,21 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     pagination_class = ProjectPagination
+    filterset_class = ProjectFilter
+
+
+class ToDoPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
+    pagination_class = ToDoPagination
+    filterset_fields = ('project',)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
