390699aeef821ce8d45c70359cc577585a390be3
Turganov Ivan <mrdurandall@gmail.com>
2022-09-21 22:21:32 +0300

    created filter for project model

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import FilterSet, CharFilter
 
 from .models import Project, ToDo
 from .serializers import ProjectSerializer, ToDoSerializer
@@ -7,6 +8,14 @@ from .serializers import ProjectSerializer, ToDoSerializer
 
 class ProjectPagination(LimitOffsetPagination):
     default_limit = 10
+
+
+class ProjectFilter(FilterSet):
+    title = CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = Project
+        fields = ['title']
 
 
 class ProjectModelViewSet(ModelViewSet):
