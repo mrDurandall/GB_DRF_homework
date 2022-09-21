da6e4132979948dbfca56b84a2a6910fa4ec29a3
Turganov Ivan <mrdurandall@gmail.com>
2022-09-21 22:03:27 +0300

    updated viewset for users

--- todo/users/views.py
+++ todo/users/views.py
@@ -1,9 +1,20 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (ListModelMixin,
+                                   RetrieveModelMixin,
+                                   UpdateModelMixin)
 
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+# class UserModelViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserModelSerializer
+
+class UserModelViewSet(ListModelMixin,
+                       RetrieveModelMixin,
+                       UpdateModelMixin,
+                       GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    
