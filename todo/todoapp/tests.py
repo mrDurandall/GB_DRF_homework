ee85506c8777b9f8c9a3ee5dd909cc906693e1ff
Turganov Ivan <mrdurandall@gmail.com>
2022-10-11 18:36:03 +0300

    created tests for project model using APIClient

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -18,9 +18,21 @@ class TestProjectModelViewSet(TestCase):
         self.login = 'super'
         self.email = 'super@mail.com'
         self.password = 'qwaszx'
-        self.project = {'title': 'Test project',
-                        'repo_link': 'http://testrepos.com/',
-                        'users': []}
+        self.project_data = {
+            'title': 'Test project',
+            'repo_link': 'http://testrepos.com/',
+            'users': []
+        }
+        self.project_data_new = {
+            'title': 'New Test project',
+            'repo_link': 'http://testrepos.com/new',
+            'users': []
+        }
+        self.project = Project.objects.create(
+            title=self.project_data['title'],
+            repo_link=self.project_data['repo_link']
+        )
+        self.admin = User.objects.create_superuser(self.login, self.email, self.password)
 
     def test_get_projects_list(self):
         factory = APIRequestFactory()
@@ -31,16 +43,47 @@ class TestProjectModelViewSet(TestCase):
 
     def test_unauthorized_create_project(self):
         factory = APIRequestFactory()
-        request = factory.post(self.url, self.project, self.format)
+        request = factory.post(self.url, self.project_data, self.format)
         view = ProjectModelViewSet.as_view({'post': 'create'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_authorized_create_project(self):
         factory = APIRequestFactory()
-        request = factory.post(self.url, self.project, self.format)
-        admin = User.objects.create_superuser(self.login, self.email, self.password)
-        force_authenticate(request, admin)
+        request = factory.post(self.url, self.project_data, self.format)
+        force_authenticate(request, self.admin)
         view = ProjectModelViewSet.as_view({'post': 'create'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_project_detail(self):
+        client = APIClient()
+        response = client.get(f'{self.url}/{self.project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unauthorized_update_project(self):
+        client = APIClient()
+        response = client.put(
+            f'{self.url}/{self.project.id}/',
+            self.project_data_new,
+            format=self.format,
+        )
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authorized_update_project(self):
+        client = APIClient()
+        client.login(username=self.login, password=self.password)
+        response = client.put(
+            f'{self.url}/{self.project.id}/',
+            self.project_data_new,
+            format=self.format,
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.title, self.project_data_new.get('title'))
+        self.assertEqual(self.project.repo_link, self.project_data_new.get('repo_link'))
+        client.logout()
