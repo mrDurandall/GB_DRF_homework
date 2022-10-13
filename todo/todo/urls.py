18c6aa368fbc3b6e50e420454e876adf7ac4f0b2
Turganov Ivan <mrdurandall@gmail.com>
2022-10-13 11:54:06 +0300

    created documentation for API with swagger and redoc

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from users.views import UserModelViewSet
@@ -26,7 +27,21 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='ToDos',
+        default_version='v1',
+        description='API documentation for ToDo project',
+        contact=openapi.Contact(email='super@mail.com'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -42,4 +57,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/users/v1', include('users.urls', namespace='v1')),
     path('api/users/v2', include('users.urls', namespace='v2')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
