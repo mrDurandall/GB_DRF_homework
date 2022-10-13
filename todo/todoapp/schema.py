c5b4a7e036467163e70926a16cdbfd678a92781b
Turganov Ivan <mrdurandall@gmail.com>
2022-10-13 22:05:47 +0300

    created graphql mutation for todo text

--- todo/todoapp/schema.py
+++ todo/todoapp/schema.py
@@ -31,6 +31,7 @@ class Query(graphene.ObjectType):
     all_projects = graphene.List(ProjectType)
     project_by_id = graphene.Field(ProjectType, id=graphene.Int(required=True))
     project_by_username = graphene.List(ProjectType, username=graphene.String(required=False))
+    todo_by_id = graphene.Field(ToDoType, id=graphene.Int(required=True))
     todos_by_project = graphene.List(ToDoType, project_id=graphene.Int(required=True))
 
     def resolve_all_projects(self, info):
@@ -45,6 +46,12 @@ class Query(graphene.ObjectType):
     def resolve_project_by_username(self, info, username=None):
         return Project.objects.filter(users__username=username)
 
+    def resolve_todo_by_id(self, info, id):
+        try:
+            return ToDo.objects.get(id=id)
+        except ToDo.DoesNotExist:
+            return None
+
     def resolve_todos_by_project(self, info, project_id):
         try:
             return ToDo.objects.filter(project__id=project_id)
@@ -52,4 +59,24 @@ class Query(graphene.ObjectType):
             return None
 
 
-schema = graphene.Schema(query=Query)
+class ToDoMutation(graphene.Mutation):
+
+    class Arguments:
+        new_text = graphene.String(required=False)
+        id = graphene.ID()
+
+    todo = graphene.Field(ToDoType)
+
+    @classmethod
+    def mutate(cls, root, info, new_text, id):
+        todo = ToDo.objects.get(pk=id)
+        todo.text = new_text
+        todo.save()
+        return ToDoMutation(todo=todo)
+
+
+class Mutation(graphene.ObjectType):
+    update_todo = ToDoMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
