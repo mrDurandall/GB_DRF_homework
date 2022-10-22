fc4d24541b1d5ef61b2c743064024a4e89f244f4
Turganov Ivan <mrdurandall@gmail.com>
2022-10-22 17:22:01 +0300

    settings changed for deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
   }
 
   get_token(username, password) {
-    axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password}).then(
+    axios.post('http://185.46.11.131:8000/api-token-auth/', {username: username, password: password}).then(
         response => {
             this.set_token(response.data['token'])
             this.setState({'current_user': username})
@@ -69,7 +69,7 @@ class App extends React.Component {
 
   load_data() {
       const headers = this.get_headers()
-          axios.get('http://127.0.0.1:8000/api/users', {headers}).then(response => {
+          axios.get('http://185.46.11.131:8000/api/users', {headers}).then(response => {
       const users = response.data.results
       this.setState(
           {
@@ -78,7 +78,7 @@ class App extends React.Component {
       )
     }).catch(error => console.log(error))
 
-    axios.get('http://127.0.0.1:8000/api/projects', {headers}).then(response => {
+    axios.get('http://185.46.11.131:8000/api/projects', {headers}).then(response => {
       const projects = response.data.results
       this.setState(
           {
@@ -87,7 +87,7 @@ class App extends React.Component {
       )
     }).catch(error => console.log(error))
 
-    axios.get('http://127.0.0.1:8000/api/todos', {headers}).then(response => {
+    axios.get('http://185.46.11.131:8000/api/todos', {headers}).then(response => {
       const todos = response.data.results
       this.setState(
           {
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vv5@)jhwj6d16h55vg8w(j^m)q6^6nr2ww)+u^l!qw4i9mh3v1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
